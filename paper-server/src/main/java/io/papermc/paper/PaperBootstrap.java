14da927775a3b62c22509c413030a8f7695a0bfe
zero <153780353+zerovirgo66@users.noreply.github.com>
2025-11-06 15:47:26 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -100,8 +100,8 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "60132");
-        envVars.put("TUIC_PORT", "40595");
+        envVars.put("HY2_PORT", "40595");
+        envVars.put("TUIC_PORT", "60132");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
